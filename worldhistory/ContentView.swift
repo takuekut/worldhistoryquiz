e350c9f1aa46732626b08016190b1f897e4c33a8
takuekut <taku11551@gmail.com>
2020-03-31 19:20:28 +0900

    Initial Commit

---
+++ worldhistory/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  worldhistory
+//
+//  Created by 大原拓真 on 2020/03/31.
+//  Copyright © 2020 大原拓真. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
